42c85ebf7348e5c3687cb8f7f0c79fed88567774
Lei <jinleix3@hotmail.com>
2024-01-02 15:11:18 +0100

    feat: show display name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pizza_ar_flutter_2023/main.dart';
+import 'package:bitz/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
