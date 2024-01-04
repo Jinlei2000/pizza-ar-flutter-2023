09cac87f10ecc474640f1099279af6b54c4bdeb0
Lei <jinleix3@hotmail.com>
2024-01-04 17:03:02 +0100

    feat: clean up code

--- lib/screens/tabs/home.dart
+++ lib/screens/tabs/home.dart
@@ -14,46 +14,51 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: MyColors.background,
-      // add a button
-      body: Container(
-        padding: const EdgeInsets.all(16),
-        child: const Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 40),
-            Text(
-              'Welcome to Bitz',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Order your favorite pizza',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            SizedBox(height: 40),
-          ],
-        ),
-      ),
-
-      // add a button
-      floatingActionButton: Container(
-        width: double.infinity,
-        margin: const EdgeInsets.symmetric(horizontal: 16),
-        child: Button(
-          text: 'Order Now',
-          onPressed: () {
-            // go to tab pizza (index 1)
-            navigateToTabIndex(1);
-          },
-        ),
-      ),
+      body: body(),
+      floatingActionButton: floatingActionButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+
+  Container body() {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 40),
+          Text(
+            'Welcome to Bitz',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            'Order your favorite pizza',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          SizedBox(height: 40),
+        ],
+      ),
+    );
+  }
+
+  Container floatingActionButton() {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      child: Button(
+        text: 'Order Now',
+        onPressed: () {
+          // go to tab pizza (index 1)
+          navigateToTabIndex(1);
+        },
+      ),
     );
   }
 }
