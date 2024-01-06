6dd1ae2734e4978d26336dbfd392dfaa05fe1466
Lei <jinleix3@hotmail.com>
2024-01-06 23:23:13 +0100

    feat: add comments

--- lib/screens/tabs/order.dart
+++ lib/screens/tabs/order.dart
@@ -4,6 +4,18 @@ import 'package:flutter/material.dart';
 class OrderPage extends StatelessWidget {
   const OrderPage({Key? key}) : super(key: key);
 
+  // List<OrderModel> orders = [];
+
+  void _getinitialData() {
+    // orders = OrderService().getOrders();
+
+    // OR
+
+    // orders = OrderModel().getOrders();
+
+    // activeOrders = OrderModel().getActiveOrders();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
