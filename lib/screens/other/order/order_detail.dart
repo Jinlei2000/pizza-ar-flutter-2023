00d7ec82ca16ec13b83f9cd02cd92fc2e4484554
Lei <jinleix3@hotmail.com>
2024-01-07 15:50:22 +0100

    feat: add order summary

--- lib/screens/other/order/order_detail.dart
+++ lib/screens/other/order/order_detail.dart
@@ -1,4 +1,5 @@
 // order_detail.dart
+import 'package:bitz/components/custom_app_bar.dart';
 import 'package:bitz/utils/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -7,13 +8,21 @@ class OrderDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: MyColors.background,
-      body: Center(
-        child: Text(
-          'Order Detail',
-          style: TextStyle(fontSize: 100),
-        ),
+    return Scaffold(
+      appBar: const CustomAppBar(
+        title: 'Order Detail',
+      ),
+      body: SingleChildScrollView(
+        child: _body(),
+      ),
+    );
+  }
+
+  Widget _body() {
+    return const Padding(
+      padding: EdgeInsets.only(left: 16, right: 16, top: 16),
+      child: Column(
+        children: [],
       ),
     );
   }
