1ec02678ce82d9d637bb5b5adb7344510ee30a46
Lei <jinleix3@hotmail.com>
2024-01-09 10:08:48 +0100

    feat: basic ar code

--- lib/screens/other/pizza/customize_pizza_ar.dart
+++ lib/screens/other/pizza/customize_pizza_ar.dart
@@ -1,5 +1,6 @@
-// customize_pizza_ar.dart
 import 'package:flutter/material.dart';
+import 'package:arkit_plugin/arkit_plugin.dart';
+import 'package:vector_math/vector_math_64.dart';
 
 class CustomizePizzaArPage extends StatelessWidget {
   const CustomizePizzaArPage({Key? key}) : super(key: key);
@@ -7,14 +8,18 @@ class CustomizePizzaArPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // add appbar with back button
       appBar: AppBar(
         leading: const BackButton(),
       ),
-
-      body: const Center(
-        child: Text('Customize Pizza AR'),
-      ),
+      body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated),
     );
+  }
+
+  void onARKitViewCreated(ARKitController arkitController) {
+    final node = ARKitNode(
+      geometry: ARKitSphere(radius: 0.1),
+      position: Vector3(0, 0, -0.5),
+    );
+    arkitController.add(node);
   }
 }
