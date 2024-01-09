aceba65b95b8264e405860583501593f34b8013a
Lei <jinleix3@hotmail.com>
2024-01-09 11:29:44 +0100

    feat: ar on spefic page

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,63 @@
-// // main.dart
-// import 'package:bitz/components/bottom_navigation.dart';
-// import 'package:bitz/screens/other/order/order_detail.dart';
-// import 'package:bitz/screens/other/pizza/customize_pizza_ar.dart';
-// import 'package:bitz/screens/other/pizza/overview_order.dart';
-// import 'package:bitz/screens/other/pizza/payment.dart';
-// import 'package:bitz/screens/tabs/home.dart';
-// import 'package:bitz/screens/tabs/order.dart';
-// import 'package:bitz/screens/tabs/pizza.dart';
-// import 'package:bitz/screens/tabs/profile.dart';
-// import 'package:bitz/utils/colors.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       // Hide debug banner
-//       debugShowCheckedModeBanner: false,
-
-//       // home: const BottomNavigation(),
-//       home: const CustomizePizzaArPage(),
-//       initialRoute: '/',
-//       routes: _routes,
-
-//       theme: _theme(),
-//     );
-//   }
-
-//   ThemeData _theme() {
-//     return ThemeData(
-//       useMaterial3: true,
-//       brightness: Brightness.light,
-//       colorScheme: const ColorScheme.light(background: MyColors.background),
-//       // set Inter as the default font
-//       fontFamily: 'Inter',
-//       appBarTheme: const AppBarTheme(
-//         systemOverlayStyle: SystemUiOverlayStyle.dark,
-//       ),
-//     );
-//   }
-
-//   // All routes in the app
-//   Map<String, WidgetBuilder> get _routes {
-//     return {
-//       '/home': (context) => const HomePage(),
-//       '/order': (context) => const OrderPage(),
-//       '/pizza': (context) => const PizzaPage(),
-//       '/profile': (context) => const ProfilePage(),
-//       '/pizza/customize': (context) => const CustomizePizzaArPage(),
-//       '/pizza/overview-order': (context) => const OverviewOrderPage(),
-//       '/pizza/payment': (context) => const PaymentPage(),
-//       '/order/detail': (context) => const OrderDetailPage(),
-//     };
-//   }
-// }
-
+// main.dart
+import 'package:bitz/components/bottom_navigation.dart';
+import 'package:bitz/screens/other/order/order_detail.dart';
+import 'package:bitz/screens/other/pizza/customize_pizza_ar.dart';
+import 'package:bitz/screens/other/pizza/overview_order.dart';
+import 'package:bitz/screens/other/pizza/payment.dart';
+import 'package:bitz/screens/tabs/home.dart';
+import 'package:bitz/screens/tabs/order.dart';
+import 'package:bitz/screens/tabs/pizza.dart';
+import 'package:bitz/screens/tabs/profile.dart';
+import 'package:bitz/utils/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:arkit_plugin/arkit_plugin.dart';
-import 'package:vector_math/vector_math_64.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MaterialApp(home: MyApp()));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(title: const Text('ARKit in Flutter')),
-        body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated),
-      );
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Hide debug banner
+      debugShowCheckedModeBanner: false,
 
-  void onARKitViewCreated(ARKitController arkitController) {
-    final node = ARKitNode(
-        geometry: ARKitSphere(radius: 0.1), position: Vector3(0, 0, -0.5));
-    arkitController.add(node);
+      // home: const BottomNavigation(),
+      home: const CustomizePizzaArPage(),
+      initialRoute: '/',
+      routes: _routes,
+
+      theme: _theme(),
+    );
+  }
+
+  ThemeData _theme() {
+    return ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.light,
+      colorScheme: const ColorScheme.light(background: MyColors.background),
+      // set Inter as the default font
+      fontFamily: 'Inter',
+      appBarTheme: const AppBarTheme(
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
+      ),
+    );
+  }
+
+  // All routes in the app
+  Map<String, WidgetBuilder> get _routes {
+    return {
+      '/home': (context) => const HomePage(),
+      '/order': (context) => const OrderPage(),
+      '/pizza': (context) => const PizzaPage(),
+      '/profile': (context) => const ProfilePage(),
+      '/pizza/customize': (context) => const CustomizePizzaArPage(),
+      '/pizza/overview-order': (context) => const OverviewOrderPage(),
+      '/pizza/payment': (context) => const PaymentPage(),
+      '/order/detail': (context) => const OrderDetailPage(),
+    };
   }
 }
