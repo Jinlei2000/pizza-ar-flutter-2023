dfe64d68d9f77e15a50d77721d1e81594ed11ca2
Lei <jinleix3@hotmail.com>
2024-01-09 10:24:34 +0100

    feat: test

--- lib/main.dart
+++ lib/main.dart
@@ -66,33 +66,22 @@ import 'package:flutter/material.dart';
 import 'package:arkit_plugin/arkit_plugin.dart';
 import 'package:vector_math/vector_math_64.dart';
 
-void main() => runApp(const MaterialApp(home: MyApp()));
+void main() {
+  runApp(const MaterialApp(home: MyApp()));
+}
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late ARKitController arkitController;
-
-  @override
-  void dispose() {
-    arkitController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(title: const Text('ARKit in Flutter')),
-      body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated));
+        appBar: AppBar(title: const Text('ARKit in Flutter')),
+        body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated),
+      );
 
   void onARKitViewCreated(ARKitController arkitController) {
-    this.arkitController = arkitController;
     final node = ARKitNode(
         geometry: ARKitSphere(radius: 0.1), position: Vector3(0, 0, -0.5));
-    this.arkitController.add(node);
+    arkitController.add(node);
   }
 }
