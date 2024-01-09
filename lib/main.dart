73ef14966e2dcbecdb069791e87b120b0c3f7dcb
Lei <jinleix3@hotmail.com>
2024-01-09 10:14:05 +0100

    feat: test ar on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,98 @@
-// main.dart
-import 'package:bitz/components/bottom_navigation.dart';
-import 'package:bitz/screens/other/order/order_detail.dart';
-import 'package:bitz/screens/other/pizza/customize_pizza_ar.dart';
-import 'package:bitz/screens/other/pizza/overview_order.dart';
-import 'package:bitz/screens/other/pizza/payment.dart';
-import 'package:bitz/screens/tabs/home.dart';
-import 'package:bitz/screens/tabs/order.dart';
-import 'package:bitz/screens/tabs/pizza.dart';
-import 'package:bitz/screens/tabs/profile.dart';
-import 'package:bitz/utils/colors.dart';
+// // main.dart
+// import 'package:bitz/components/bottom_navigation.dart';
+// import 'package:bitz/screens/other/order/order_detail.dart';
+// import 'package:bitz/screens/other/pizza/customize_pizza_ar.dart';
+// import 'package:bitz/screens/other/pizza/overview_order.dart';
+// import 'package:bitz/screens/other/pizza/payment.dart';
+// import 'package:bitz/screens/tabs/home.dart';
+// import 'package:bitz/screens/tabs/order.dart';
+// import 'package:bitz/screens/tabs/pizza.dart';
+// import 'package:bitz/screens/tabs/profile.dart';
+// import 'package:bitz/utils/colors.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       // Hide debug banner
+//       debugShowCheckedModeBanner: false,
+
+//       // home: const BottomNavigation(),
+//       home: const CustomizePizzaArPage(),
+//       initialRoute: '/',
+//       routes: _routes,
+
+//       theme: _theme(),
+//     );
+//   }
+
+//   ThemeData _theme() {
+//     return ThemeData(
+//       useMaterial3: true,
+//       brightness: Brightness.light,
+//       colorScheme: const ColorScheme.light(background: MyColors.background),
+//       // set Inter as the default font
+//       fontFamily: 'Inter',
+//       appBarTheme: const AppBarTheme(
+//         systemOverlayStyle: SystemUiOverlayStyle.dark,
+//       ),
+//     );
+//   }
+
+//   // All routes in the app
+//   Map<String, WidgetBuilder> get _routes {
+//     return {
+//       '/home': (context) => const HomePage(),
+//       '/order': (context) => const OrderPage(),
+//       '/pizza': (context) => const PizzaPage(),
+//       '/profile': (context) => const ProfilePage(),
+//       '/pizza/customize': (context) => const CustomizePizzaArPage(),
+//       '/pizza/overview-order': (context) => const OverviewOrderPage(),
+//       '/pizza/payment': (context) => const PaymentPage(),
+//       '/order/detail': (context) => const OrderDetailPage(),
+//     };
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:arkit_plugin/arkit_plugin.dart';
+import 'package:vector_math/vector_math_64.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MaterialApp(home: MyApp()));
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Hide debug banner
-      debugShowCheckedModeBanner: false,
+  _MyAppState createState() => _MyAppState();
+}
 
-      // home: const BottomNavigation(),
-      home: const CustomizePizzaArPage(),
-      initialRoute: '/',
-      routes: _routes,
+class _MyAppState extends State<MyApp> {
+  late ARKitController arkitController;
 
-      theme: _theme(),
-    );
+  @override
+  void dispose() {
+    arkitController.dispose();
+    super.dispose();
   }
 
-  ThemeData _theme() {
-    return ThemeData(
-      useMaterial3: true,
-      brightness: Brightness.light,
-      colorScheme: const ColorScheme.light(background: MyColors.background),
-      // set Inter as the default font
-      fontFamily: 'Inter',
-      appBarTheme: const AppBarTheme(
-        systemOverlayStyle: SystemUiOverlayStyle.dark,
-      ),
-    );
-  }
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(title: const Text('ARKit in Flutter')),
+      body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated));
 
-  // All routes in the app
-  Map<String, WidgetBuilder> get _routes {
-    return {
-      '/home': (context) => const HomePage(),
-      '/order': (context) => const OrderPage(),
-      '/pizza': (context) => const PizzaPage(),
-      '/profile': (context) => const ProfilePage(),
-      '/pizza/customize': (context) => const CustomizePizzaArPage(),
-      '/pizza/overview-order': (context) => const OverviewOrderPage(),
-      '/pizza/payment': (context) => const PaymentPage(),
-      '/order/detail': (context) => const OrderDetailPage(),
-    };
+  void onARKitViewCreated(ARKitController arkitController) {
+    this.arkitController = arkitController;
+    final node = ARKitNode(
+        geometry: ARKitSphere(radius: 0.1), position: Vector3(0, 0, -0.5));
+    this.arkitController.add(node);
   }
 }
--- lib/screens/other/pizza/customize_pizza_ar.dart
+++ lib/screens/other/pizza/customize_pizza_ar.dart
@@ -1,6 +1,5 @@
+// customize_pizza_ar.dart
 import 'package:flutter/material.dart';
-import 'package:arkit_plugin/arkit_plugin.dart';
-import 'package:vector_math/vector_math_64.dart';
 
 class CustomizePizzaArPage extends StatelessWidget {
   const CustomizePizzaArPage({Key? key}) : super(key: key);
@@ -8,18 +7,14 @@ class CustomizePizzaArPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // add appbar with back button
       appBar: AppBar(
         leading: const BackButton(),
       ),
-      body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated),
-    );
-  }
 
-  void onARKitViewCreated(ARKitController arkitController) {
-    final node = ARKitNode(
-      geometry: ARKitSphere(radius: 0.1),
-      position: Vector3(0, 0, -0.5),
+      body: const Center(
+        child: Text('Customize Pizza AR'),
+      ),
     );
-    arkitController.add(node);
   }
 }
